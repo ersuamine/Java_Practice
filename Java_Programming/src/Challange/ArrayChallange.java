4649134e781fe15c80defbb002f555376d962c9c
Amine Ersu <160289484+ersuamine@users.noreply.github.com>
2024-05-04 10:26:04 -0400

    Updated ReverseSentence.java String->Stringbuilder

--- Java_Programming/src/Challange/ArrayChallange.java
+++ Java_Programming/src/Challange/ArrayChallange.java
@@ -1,6 +1,6 @@
 package Challange;
 
-import java.util.Arrays;
+
 
 public class ArrayChallange {
     public static void main(String[] args) {
--- Java_Programming/src/java_arraylist/ReverseSentence.java
+++ Java_Programming/src/java_arraylist/ReverseSentence.java
@@ -4,14 +4,14 @@ public class ReverseSentence {
     public static void main(String[] args) {
         String str= "I love coffee";
         String[] array = str.split(" ");
-        String reversed = "";
+        StringBuilder reversed = new StringBuilder();
 
         //concatenate each word into reversed variable by going from last index to first
         for (int i = array.length-1; i >=0; i--) {
             //concatenated and add the missing space
-            reversed = reversed + array[i] + " ";
+            reversed.append(array[i]).append(" ");
         }
         //trim the string to remove extra space that has been added during the last iteration of the loop
-        System.out.println(reversed.trim());
+        System.out.println(reversed.toString().trim());
     }
 }
