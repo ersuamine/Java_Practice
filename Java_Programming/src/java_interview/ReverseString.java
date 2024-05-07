4cf5b95dbd8d21d0fde7b1eb8ad22a2cdb8d1da4
Amine Ersu <160289484+ersuamine@users.noreply.github.com>
2024-05-07 13:19:01 -0400

    Updated ReverseString.java with comments

--- Java_Programming/src/java_interview/ReverseString.java
+++ Java_Programming/src/java_interview/ReverseString.java
@@ -10,6 +10,9 @@ public class ReverseString {
 
     public static String reverseString(String str){
         String reversed = "";
+        //concatenate each letter from last index to first index
+
+        //can also use stringbuilder
         for (int i = str.length()-1; i >=0 ; i--) {
             reversed += str.charAt(i);
         }
